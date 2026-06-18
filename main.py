eda9a58786f20c82dfff64b8331b561d90d7c333
Fasco17 <abdulllahbashaev@gmail.com>
2026-06-18 18:16:59 +0200

    add new endpoints for income and expence

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
 BALANCE = {}
+
+
+class OperationRequest(BaseModel):
+    wallet_name: str
+    amount: int
+    description: str | None = None
 
 
 @app.get("/balance")
@@ -16,25 +23,47 @@ def get_balance(wallet_name: str | None = None):
 
     # Проверяем существует ли запрашиваем кошелек
     if wallet_name not in BALANCE:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Wallet '{wallet_name}' not found"
-            )
+        raise HTTPException(status_code=404, detail=f"Wallet '{wallet_name}' not found")
     # Возвращаем баланс конкретного кошелька
     return {"wallet": wallet_name, "balance": BALANCE[wallet_name]}
 
 
-@app.post("wallets/{name}")
-def receive_money(name: str, amount: int):
-    # Если кошелька с таким именем еще нет - создаем его с балансем 0
-    if name not in BALANCE:
-        BALANCE[name] = 0
-
-    # Если есть - то добавляем сумму к балансу
-    BALANCE[name] += amount
-    # Возвращаем информацию о кошельке
+@app.post("/wallets/{name}")
+def create_wallet(name: str, initial_balance: float = 0):
+    # Проверяем не существует ли уже такой кошелек
+    if name in BALANCE:
+        raise HTTPException(status_code=400, detail=f"Wallet '{name}' already exists")
+    # Создать новый кошелек с начальным балансом
+    BALANCE[name] = initial_balance
+    # Возвращать информацию о созданном кошельке
     return {
-        "message": f"Added {amount} to {name}",
+        "message": f"Wallet '{name}' created",
         "wallet": name,
-        "new_balance": BALANCE[name]
+        "balance": BALANCE[name],
     }
+
+
+@app.post("/operations/income")
+def add_income(operation: OperationRequest):
+    # Проверяем существует ли кошелек
+    if operation.wallet_name is None or operation.wallet_name not in BALANCE:
+        raise HTTPException(
+            status_code=404, detail=f"Wallet '{operation.wallet_name}' not found"
+        )
+    # Проверяем что сумму положительна
+    if operation.amount <= 0:
+        raise HTTPException(status_code=400, detail=f"Amount must be positive")
+    # Добавить доход к балансу кошелька
+    BALANCE[operation.wallet_name] += operation.amount
+    #  Возвращаем информацию о балансе
+    return {
+        "message": "Income added",
+        "wallet": operation.wallet_name,
+        "amount": operation.amount,
+        "description": operation.description,
+        "new_balance": BALANCE[operation.wallet_name]
+    }
+
+@app.post("/operations/expense")
+def add_expense():
+    pass
